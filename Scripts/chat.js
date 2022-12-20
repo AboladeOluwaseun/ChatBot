e682693e75458526a93c0e8da056b48cceedcec3
Maxibrain <aboladeoluwaseungodwithus@gmail.com>
2022-12-20 21:22:36 +0100

    automated response added

--- Scripts/chat.js
+++ Scripts/chat.js
@@ -1,40 +1,49 @@
-const toggler = document.querySelector('.toggler')
-const content =document.getElementsByClassName('content')[0]
-const userInput = document.getElementById('textInput')
-const chatBox= document.getElementById('chatbox')
+const toggler = document.querySelector(".toggler");
+const content = document.getElementsByClassName("content")[0];
+const userInput = document.getElementById("textInput");
+const chatBox = document.getElementById("chatbox");
+
 //Toggle chatbox
-toggler.addEventListener('click', ()=>{
-    if(content.style.display === ''|| content.style.display === 'none'){
-        content.style.display = 'block'
-    }else { content.style.display = 'none'}
-})
+toggler.addEventListener("click", () => {
+  if (content.style.display === "" || content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+});
 
-const getBotResponse= (userInputValue)=>{
-    if(userInputValue==='hello'){
-    const botChatHtml= document.createElement('p')
-    const span = document.createElement('span')
-    botChatHtml.classList.add('botText')
-    span.innerText=`${'hi seun'}`
-    botChatHtml.appendChild(span)
-    chatBox.appendChild(botChatHtml)
-    }
-    
-}
 //renderUserMessage
-const renderUserMessage =(userInputValue)=>{
-    const userChatHtml= document.createElement('p')
-    const span = document.createElement('span')
-    userChatHtml.classList.add('userText')
-    span.innerText=`${userInputValue}`
-    userChatHtml.appendChild(span)
-    chatBox.appendChild(userChatHtml)
-    getBotResponse(userInputValue)
-    
-    // document.getElementById('chat-bar-bottom').scrollIntoView(true)
-}
+const renderUserMessage = (userInputValue) => {
+  renderMessage(userInputValue, "userText");
+  setTimeout(() => {
+    renderBotMessage(userInputValue);
+  }, 600);
+};
 
-const sendButton=()=>{
-    renderUserMessage(userInput.value)
-    userInput.value=''
-}
+//get a response from ChatBot
+const getBotMessage = (userInputValue) => {
+  return responses[userInputValue] === undefined
+    ? "I have no response to this"
+    : responses[userInputValue];
+};
+// render message from bot
+const renderBotMessage = (userMessage) => {
+  const res = getBotMessage(userMessage);
+  renderMessage(res, "botText");
+};
 
+// logic for rendering messages
+const renderMessage = (message, className) => {
+  const response = getBotMessage(userInput.value);
+  const botChatHtml = document.createElement("p");
+  const span = document.createElement("span");
+  botChatHtml.classList.add(className);
+  span.innerText = `${message}`;
+  botChatHtml.appendChild(span);
+  chatBox.appendChild(botChatHtml);
+};
+
+const sendButton = () => {
+  renderUserMessage(userInput.value);
+  userInput.value = "";
+};
---
+++ Scripts/responses.js
@@ -0,0 +1,6 @@
+const responses = {
+  hello: "hey, how are you doing?,how can I help You",
+  hi: "hey, how are you doing?,how can I help You",
+  hey: "hey, how are you doing?,how can I help You",
+  umar: "hi umar",
+};
